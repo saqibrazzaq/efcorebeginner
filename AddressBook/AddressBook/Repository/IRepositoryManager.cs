b75f9cae3993ec9bba040b75d3cfd7e370973cbb
saqibrazzaq <saqibrazzaq@gmail.com>
2023-01-24 18:32:03 +0500

    person repository add in repository manager

--- AddressBook/AddressBook/Repository/IRepositoryManager.cs
+++ AddressBook/AddressBook/Repository/IRepositoryManager.cs
@@ -7,6 +7,18 @@
         ICityRepository CityRepository { get; }
         ITranslationRepository TranslationRepository { get; }
         ITimezoneRepository TimezoneRepository { get; }
+        IPersonRepository PersonRepository { get; }
+        IPersonLabelRepository PersonLabelRepository { get; }
+        IPersonEmailRepository PersonEmailRepository { get; }
+        IEmailLabelRepository EmailLabelRepository { get; }
+        IPersonPhoneRepository PersonPhoneRepository { get; }
+        IPhoneLabelRepository PhoneLabelRepository { get; }
+        IPersonAddressRepository PersonAddressRepository { get; }
+        IAddressLabelRepository AddressLabelRepository { get; }
+        IPersonWebsiteRepository PersonWebsiteRepository { get; }
+        IWebsiteLabelRepository WebsiteLabelRepository { get; }
+        IPersonChatRepository PersonChatRepository { get; }
+        IChatLabelRepository ChatLabelRepository { get; }
         void Save();
     }
 }
--- AddressBook/AddressBook/Repository/RepositoryManager.cs
+++ AddressBook/AddressBook/Repository/RepositoryManager.cs
@@ -11,6 +11,18 @@ namespace AddressBook.Repository
         private readonly Lazy<ICityRepository> _cityRepository;
         private readonly Lazy<ITranslationRepository> _translationRepository;
         private readonly Lazy<ITimezoneRepository> _timezoneRepository;
+        private readonly Lazy<IPersonRepository> _personRepository;
+        private readonly Lazy<IPersonLabelRepository> _personLabelRepository;
+        private readonly Lazy<IPersonEmailRepository> _personEmailRepository;
+        private readonly Lazy<IEmailLabelRepository> _emailLabelRepository;
+        private readonly Lazy<IPersonPhoneRepository> _personPhoneRepository;
+        private readonly Lazy<IPhoneLabelRepository> _phoneLabelRepository;
+        private readonly Lazy<IPersonAddressRepository> _personAddressRepository;
+        private readonly Lazy<IAddressLabelRepository> _addressLabelRepository;
+        private readonly Lazy<IPersonWebsiteRepository> _personWebsiteRepository;
+        private readonly Lazy<IWebsiteLabelRepository> _websiteLabelRepository;
+        private readonly Lazy<IPersonChatRepository> _personChatRepository;
+        private readonly Lazy<IChatLabelRepository> _chatLabelRepository;
         public RepositoryManager(AppDbContext context)
         {
             _context = context;
@@ -26,6 +38,30 @@ namespace AddressBook.Repository
                 new TranslationRepository(context));
             _timezoneRepository = new Lazy<ITimezoneRepository>(() =>
                 new TimezoneRepository(context));
+            _personRepository = new Lazy<IPersonRepository>(() =>
+                new PersonRepository(context));
+            _personLabelRepository = new Lazy<IPersonLabelRepository>(() =>
+                new PersonLabelRepository(context));
+            _personEmailRepository = new Lazy<IPersonEmailRepository>(() =>
+                new PersonEmailRepository(context));
+            _emailLabelRepository = new Lazy<IEmailLabelRepository>(() =>
+                new EmailLabelRepository(context));
+            _personPhoneRepository = new Lazy<IPersonPhoneRepository>(() =>
+                new PersonPhoneRepository(context));
+            _phoneLabelRepository = new Lazy<IPhoneLabelRepository>(() =>
+                new PhoneLabelRepository(context));
+            _personAddressRepository = new Lazy<IPersonAddressRepository>(() =>
+                new PersonAddressRepository(context));
+            _addressLabelRepository = new Lazy<IAddressLabelRepository>(() =>
+                new AddressLabelRepository(context));
+            _personWebsiteRepository = new Lazy<IPersonWebsiteRepository>(() =>
+                new PersonWebsiteRepository(context));
+            _websiteLabelRepository = new Lazy<IWebsiteLabelRepository>(() =>
+                new WebsiteLabelRepository(context));
+            _personChatRepository = new Lazy<IPersonChatRepository>(() =>
+                new PersonChatRepository(context));
+            _chatLabelRepository = new Lazy<IChatLabelRepository>(() =>
+                new ChatLabelRepository(context));
         }
 
         public ICountryRepository CountryRepository => _countryRepository.Value;
@@ -33,6 +69,18 @@ namespace AddressBook.Repository
         public ICityRepository CityRepository => _cityRepository.Value;
         public ITranslationRepository TranslationRepository => _translationRepository.Value;
         public ITimezoneRepository TimezoneRepository => _timezoneRepository.Value;
+        public IPersonRepository PersonRepository => _personRepository.Value;
+        public IPersonLabelRepository PersonLabelRepository => _personLabelRepository.Value;
+        public IPersonEmailRepository PersonEmailRepository => _personEmailRepository.Value;
+        public IEmailLabelRepository EmailLabelRepository => _emailLabelRepository.Value;
+        public IPersonPhoneRepository PersonPhoneRepository => _personPhoneRepository.Value;
+        public IPhoneLabelRepository PhoneLabelRepository => _phoneLabelRepository.Value;
+        public IPersonAddressRepository PersonAddressRepository => _personAddressRepository.Value;
+        public IAddressLabelRepository AddressLabelRepository => _addressLabelRepository.Value;
+        public IPersonWebsiteRepository PersonWebsiteRepository => _personWebsiteRepository.Value;
+        public IWebsiteLabelRepository WebsiteLabelRepository => _websiteLabelRepository.Value;
+        public IPersonChatRepository PersonChatRepository => _personChatRepository.Value;
+        public IChatLabelRepository ChatLabelRepository => _chatLabelRepository.Value;
 
         public void Save()
         {
